101b36c567082f6bd08abe40436202902653d6d5
Yngve Høiseth <yhoiseth@gmail.com>
2017-06-15 17:26:58 +0200

    Extract navigation

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,17 +19,7 @@
 
 <body>
 
-<div class="blog-masthead">
-    <div class="container">
-        <nav class="nav blog-nav">
-            <a class="nav-link active" href="#">Home</a>
-            <a class="nav-link" href="#">New features</a>
-            <a class="nav-link" href="#">Press</a>
-            <a class="nav-link" href="#">New hires</a>
-            <a class="nav-link" href="#">About</a>
-        </nav>
-    </div>
-</div>
+@include('partials.navigation')
 
 <div class="blog-header">
     <div class="container">
